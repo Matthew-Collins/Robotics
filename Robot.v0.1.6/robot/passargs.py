88ef390e3276e6404a9e2119fb8b97869bbf522e
Matthew-Collins <matthew.collins@cwgsy.net>
2016-08-13 09:55:20 +0100

    Restructure Folders

--- Robot.v0.1.6/robot/passargs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import argparse
- 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s','--speed')
-args = parser.parse_args()
- 
-print (args.speed)
--- Robot.v0.1.7/robot/stop.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import time,initio 
-
-initio.stop
-initio.cleanup()
-    
