82a27768f9dfab59489faf10ff9a39fb5b604d39
Matthew-Collins <matthew.collins@cwgsy.net>
2017-01-29 20:30:32 +0000

    New Windows 10 IoT Controller

---
+++ Hardware/4tronix/PiconZero/VB.Net/PiconZero/MainPage.xaml.vb
@@ -0,0 +1,20 @@
+﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+    Private Sub ButtonUp_Click(sender As Object, e As RoutedEventArgs) Handles ButtonUp.Click
+
+    End Sub
+    Private Sub ButtonLeft_Click(sender As Object, e As RoutedEventArgs) Handles ButtonLeft.Click
+
+    End Sub
+    Private Sub ButtonRight_Click(sender As Object, e As RoutedEventArgs) Handles ButtonRight.Click
+
+    End Sub
+    Private Sub ButtonDown_Click(sender As Object, e As RoutedEventArgs) Handles ButtonDown.Click
+
+    End Sub
+End Class
