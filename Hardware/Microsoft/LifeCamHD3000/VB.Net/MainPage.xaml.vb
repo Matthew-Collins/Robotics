930e5825bf9305b5f1223b8ba37c510fbbfa1826
Matthew-Collins <matthew.collins@cwgsy.net>
2017-02-03 13:40:26 +0000

    Basic WebCam Viewer using Microsoft LifeCam HD-3000

---
+++ Hardware/Microsoft/LifeCamHD3000/VB.Net/MainPage.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Windows.Media.Capture
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private WebCam As New MediaCapture
+
+    Private Async Sub MainPage_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Await WebCam.InitializeAsync
+        Me.WebCamView.Source = Me.WebCam
+        Await WebCam.StartPreviewAsync
+    End Sub
+
+End Class
