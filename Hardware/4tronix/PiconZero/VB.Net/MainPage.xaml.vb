3b7cdbc75b4c885f3db9b01696c83d0d61b437e2
Matthew-Collins <matthew.collins@cwgsy.net>
2017-02-04 12:00:49 +0000

    Ultrasonic Distance Sensor - Work-in-Progress

--- Hardware/4tronix/PiconZero/VB.Net/MainPage.xaml.vb
+++ Hardware/4tronix/PiconZero/VB.Net/MainPage.xaml.vb
@@ -7,13 +7,16 @@ Public NotInheritable Class MainPage
     Inherits Page
 
     Private Picon As PiconZero
+    Private Sonic As Ultrasonic
+    Private WithEvents Timer As New DispatcherTimer With {.Interval = New TimeSpan(0, 0, 3)}
 
     Public Sub New()
         InitializeComponent()
     End Sub
     Private Async Sub MainPage_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         Me.Picon = Await PiconZero.NewAsync
-        Me.Status.Text = "Ready"
+        Me.Sonic = New Ultrasonic()
+        'Me.Timer.Start()
     End Sub
     Private Sub Channels_ValueChanged(sender As Object, e As RangeBaseValueChangedEventArgs) Handles Channel0.ValueChanged, Channel1.ValueChanged, Channel2.ValueChanged, Channel3.ValueChanged, Channel4.ValueChanged, Channel5.ValueChanged
         With DirectCast(sender, Slider)
@@ -28,4 +31,8 @@ Public NotInheritable Class MainPage
         Me.Channel4.Value = 0
         Me.Channel5.Value = 0
     End Sub
+    Private Sub Timer_Tick(sender As Object, e As Object) Handles Timer.Tick
+        Me.Distance.Text = $"Distance: {Me.Sonic.GetDistance} at {DateTime.Now:HH:mm:ss}"
+    End Sub
+
 End Class
