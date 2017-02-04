9bf176570e8a912fe450757fc385169f09470197
Matthew-Collins <matthew.collins@cwgsy.net>
2017-02-04 17:27:44 +0000

    One Wheel Working

--- Robots/R3/BalanceBot/StartupTask.vb
+++ Robots/R3/BalanceBot/StartupTask.vb
@@ -4,21 +4,39 @@ Imports System.Linq
 Imports System.Text
 Imports System.Net.Http
 Imports Windows.ApplicationModel.Background
-
-' The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+Imports Windows.System.Threading
+Imports AdafruitClassLibrary
 
 Public NotInheritable Class StartupTask
     Implements IBackgroundTask
 
-    Public Sub Run(taskInstance As IBackgroundTaskInstance) Implements IBackgroundTask.Run
+    Private MotorHat As MotorHat
+    Private Stepper As MotorHat.Stepper
 
-        ' 
-        ' TODO: Insert code to perform background work
-        '
-        ' If you start any asynchronous methods here, prevent the task
-        ' from closing prematurely by using BackgroundTaskDeferral as
-        ' described in http://aka.ms/backgroundtaskdeferral
-        '
+    Public Async Sub Run(taskInstance As IBackgroundTaskInstance) Implements IBackgroundTask.Run
+
+        Dim Deferral = taskInstance.GetDeferral()
+
+        Me.MotorHat = New MotorHat()
+        Await MotorHat.InitAsync(1600).ConfigureAwait(False)
+
+        Me.Stepper = MotorHat.GetStepper(200, 1)
+        Me.Stepper.SetSpeed(200)
+
+        Stepper.step(200, MotorHat.Stepper.Command.FORWARD, MotorHat.Stepper.Style.SINGLE)
+        Stepper.step(200, MotorHat.Stepper.Command.BACKWARD, MotorHat.Stepper.Style.SINGLE)
+
+        Stepper.step(200, MotorHat.Stepper.Command.FORWARD, MotorHat.Stepper.Style.DOUBLE)
+        Stepper.step(200, MotorHat.Stepper.Command.BACKWARD, MotorHat.Stepper.Style.DOUBLE)
+
+        Stepper.step(200, MotorHat.Stepper.Command.FORWARD, MotorHat.Stepper.Style.INTERLEAVE)
+        Stepper.step(200, MotorHat.Stepper.Command.BACKWARD, MotorHat.Stepper.Style.INTERLEAVE)
+
+        'Stepper.step(10, MotorHat.Stepper.Command.FORWARD, MotorHat.Stepper.Style.MICROSTEP)
+        'Stepper.step(10, MotorHat.Stepper.Command.BACKWARD, MotorHat.Stepper.Style.MICROSTEP)
+
+        Stepper.Release()
 
     End Sub
+
 End Class
