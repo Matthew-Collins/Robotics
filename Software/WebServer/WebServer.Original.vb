b654ea60474bf2d726cafccf5bcc3e2ab3367e2b
Matthew-Collins <matthew.collins@cwgsy.net>
2017-02-09 17:56:19 +0000

    Basic WebServer Running on Pi via NETStandard.HttpListener

---
+++ Software/WebServer/WebServer.Original.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Text
+Imports Windows.Networking.Sockets
+Imports Windows.Storage.Streams
+
+Public Class WebServer
+    Private Const BufferSize = 1024 * 8
+    Private WithEvents Listener As New StreamSocketListener
+    Public Async Function StartAsync() As Task
+        Await Listener.BindServiceNameAsync("8081").AsTask
+    End Function
+    Private Async Sub Listener_ConnectionReceived(sender As StreamSocketListener, args As StreamSocketListenerConnectionReceivedEventArgs) Handles Listener.ConnectionReceived
+
+        Dim Request As New StringBuilder
+
+        Using Input = args.Socket.InputStream
+            Dim Data(BufferSize - 1) As Byte
+            Dim Buffer = Data.AsBuffer
+            Dim DataRead = CUInt(BufferSize)
+            While DataRead = BufferSize
+                Await Input.ReadAsync(Buffer, BufferSize, InputStreamOptions.Partial)
+                Request.Append(Encoding.UTF8.GetString(Data, 0, Data.Length))
+                DataRead = Buffer.Length
+            End While
+        End Using
+
+        Using Output = args.Socket.OutputStream
+            Using Response = Output.AsStreamForWrite
+
+                Dim Page As New IO.StringWriter
+                Page.WriteLine("Hello World")
+
+                Dim BodyArray = Encoding.UTF8.GetBytes(Page.ToString)
+                Dim BodyStream = New MemoryStream(BodyArray)
+
+                Dim Header = $"HTTP/1.1 200 OK{vbCrLf}Content-Length: {BodyStream.Length}{vbCrLf}Connection: close{vbCrLf}{vbCrLf}"
+                Dim HeaderArray = Encoding.UTF8.GetBytes(Header)
+
+                Await Response.WriteAsync(HeaderArray, 0, HeaderArray.Length)
+                Await BodyStream.CopyToAsync(Response)
+                Await Response.FlushAsync()
+
+            End Using
+        End Using
+
+    End Sub
+
+End Class
---
+++ Software/WebServer/WebServer.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net
+Imports System.Net.Http
+
+Public NotInheritable Class WebServer
+    Private Deferral As Background.BackgroundTaskDeferral
+    Private WithEvents Listener As New HttpListener(IPAddress.Parse("192.168.1.107"), 8081)
+    Public Sub New(Deferral As Background.BackgroundTaskDeferral)
+        Me.Deferral = Deferral
+    End Sub
+    Public Sub Start()
+        Me.Listener.Start()
+    End Sub
+    Public Sub [Stop]()
+        Me.Listener.Close()
+    End Sub
+    Private Async Sub Listener_Request(sender As Object, e As HttpListenerRequestEventArgs) Handles Listener.Request
+        If e.Request.Method = HttpMethods.Get Then
+            Await e.Response.WriteContentAsync($"Hello from Server at: {DateTime.Now}{vbCrLf}")
+        Else
+            e.Response.MethodNotAllowed()
+        End If
+        e.Response.Close()
+    End Sub
+End Class
